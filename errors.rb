153c23821cd79c11c6d8ede9cecd27da2f2825ec
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-05-21 18:35:15 +0700

    Added simple syntax checker

--- errors.rb
+++ errors.rb
@@ -21,3 +21,11 @@ end
 class InvalidArgumentError < Error
   @@msg = "argument '%s' is neither integer nor label"
 end
+
+class InvalidLabelError < Error
+  @@msg = "label '%s' is invalid"
+end
+
+class InvalidOpError < Error
+  @@msg = "instruction '%s' is invalid"
+end
--- line.rb
+++ line.rb
@@ -2,8 +2,16 @@ require "errors.rb"
 
 class Line
   attr_reader :label, :op, :arg, :number
+  LABEL = /^[_A-Za-z][_A-Za-z0-9]*$/
+  OP = /^[_A-Za-z][_A-Za-z0-9]*$/
+  ARG_NUM = /^[0-9]*$/
   
   def initialize(label, op, arg, number)
+    op ||= ""
+    arg ||= "0"
+    raise InvalidLabelError, [label, number] unless not label or label =~ LABEL
+    raise InvalidOpError, [op, number] unless op =~ OP
+    raise InvalidArgumentError, [arg, number] unless arg =~ ARG_NUM or arg =~ LABEL
     @label, @op, @arg, @number = label, op, arg, number
   end
 
@@ -26,7 +34,6 @@ def parse2lines(str)
     # add nil as label if no label
     parts.unshift( nil ) if ["\t", " "].include? line[0..0]
 
-    # TODO add own errors
     raise UnexpectedStuffAtTheEndError, [parts[3], number + 1] if parts.count > 3
     lines << Line.new( parts[0], parts[1], parts[2], number + 1 )
   end
